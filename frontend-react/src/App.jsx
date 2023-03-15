82e428915b7f918e081cf640a8772ae3b8a7e541
Parth Manaktala <manaktala.parth@gmail.com>
2023-03-15 16:34:59 -0400

    G1-1|G1-2|Added Landing Page,Nav Bar and router routes

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,9 +1,19 @@
-import './index.css';
+import "./index.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => (
-    <>
-        <h1>Welcome to Mastodon Campus Store</h1>
-    </>
+  <>
+    <Header />
+    <Container>
+      <main className="py-3">
+        <HomeScreen />
+      </main>
+    </Container>
+    <Footer />
+  </>
 );
 
 export default App;
