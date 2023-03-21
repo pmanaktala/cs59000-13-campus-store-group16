d4eca2d47c5f309f45ac38ad01b2ce7383d5f20b
poorencoder <poorencoder@users.noreply.github.com>
2023-03-21 14:26:38 -0400

    G1-8 | SC

--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./bootstrap.min.css";
+import { Provider } from "react-redux";
+
+import store from "./store";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 reportWebVitals();
---
+++ frontend-react/src/store.js
@@ -0,0 +1,26 @@
+import { configureStore, combineReducers, applyMiddleware } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { ProductListReducer, ProductDetailsReducer} from './reducers/reducers'
+
+
+
+
+const reducer = combineReducers({
+  productList: ProductListReducer,
+  productDetails : ProductDetailsReducer ,
+})
+
+const initialState = {
+  products: [],
+  error: null,
+  loading: false,
+}
+const middleware = [thunk]
+
+const store = configureStore(
+  { reducer, initialState },
+  composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store
