ac655281a004aab95b2fc978521da0fc11320995
Shreya Vithala <v01@pfw.edu>
2023-04-23 12:45:25 -0400

    added payment screen method in app

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -10,6 +10,7 @@ import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
 
 const App = () => (
   <Router>
@@ -20,6 +21,7 @@ const App = () => (
           <Route path="/" element={<HomeScreen />} exact />
           <Route path="/register" element={<RegisterScreen />} exact />
           <Route path="/shipping" element={<ShippingScreen />} />
+          <Route path="/payment" element={<PaymentScreen />} />
           <Route path="/profile" element={<ProfileScreen />} exact />
           <Route path="/product/:id" element={<ProductScreen />} />
           <Route path="/cart/:id?" element={<CartScreen />} />
