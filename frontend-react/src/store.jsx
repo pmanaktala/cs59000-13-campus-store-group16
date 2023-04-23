8595e1a746d697d8a073bba901d9b46c2dfbfadb
Shreya Vithala <v01@pfw.edu>
2023-04-23 13:37:03 -0400

    added order reducers

--- frontend-react/src/store.jsx
+++ frontend-react/src/store.jsx
@@ -16,6 +16,7 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer,
 } from "./reducers/userReducers";
+import { orderCreateReducer } from "./reducers/orderReducers";
 
 const reducer = combineReducers({
   productList: ProductListReducer,
@@ -25,6 +26,7 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  orderCreate: orderCreateReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
