16d8940db9c1cd9a9b86842df88f7014047295bc
Parth Manaktala <manaktala.parth@gmail.com>
2023-04-24 20:37:55 -0400

    Changed project structure for deployment

---
+++ data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: "PFW Mastodons Banded Sweatpants",
+    image: "/images/lower.png",
+    description:
+      "The Champion Eco Power blend Banded Bottom Pant provides ultimate comfort. Brushed inside to make them super soft, these pants are even better because they are made with recycled polyester fibers. Show Mastodons pride while being stylish, comfortable, and eco-friendly all at the same time. These pants feature a 31'' inseam, inside drawcord at waistband, and covered elastic ankle. There are also side pockets for keeping your id, cash, keys and game day tickets. Screen Printed Purdue University Fort Wayne graphics.",
+    brand: "Champion",
+    category: "Clothing",
+    price: 38.0,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: "PFW 84x54 Mastodons Blanket",
+    image: "/images/blanket.png",
+    description:
+      "This large, heavyweight Sweatshirt Blanket is extra-soft like your favorite sweatshirt. Features a large logo on primary team color fabric. Snuggle up in team spirit with this high quality blanket. Cozy up, this blanket is large enough to share. Machine washable. 84'' x 54''. Screen printed.",
+    brand: "Logo Brands",
+    category: "Accessories",
+    price: 22.0,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: "PFW Grey Hoodie @Champion ",
+    image: "/images/hoodie.png",
+    description:
+      "Stay warm -- and show that pride! The screen-printed graphic on the front of this hooded sweatshirt will communicate your school allegiance while you stay cozy in soft cotton/polyester fabric. Attached drawstring hood. Front kangaroo pocket. Rib-knit cuffs and bottom hem.",
+    brand: "Champion",
+    category: "Clothing",
+    price: 49.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: "Hat with a Mastodon Logo",
+    image: "/images/hat.png",
+    description:
+      "Top off a casual look with this easy twill campus hat. This washed cotton twill cap features an unstructured relaxed fit, a self-fabric sweatband, eyelets for ventilation, a curved front bill, and an adjustable back strap for a custom fit. You're a-head of the pack with the small school mascot logo embroidered on the front.",
+    brand: "Champion",
+    category: "Clothing",
+    price: 26.89,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    name: "Purdue University Fort Wayne 16 oz. Tumbler",
+    image: "/images/tumbler.png",
+    description:
+      "You've already color-coordinated your clothes and accessories, now make sure you've got the drinkware to match with the 16 ounce ColorMatch Tumbler. This matte-finished, double-walled tumbler is available in multiple bright colors, has a slim, tapered design, a sturdy acrylic lid, and best of all, has a matching color straw.",
+    brand: "LXG",
+    category: "Accessories",
+    price: 22.0,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    name: "Purdue University Fort Wayne Mastodons Plush",
+    image: "/images/mastodon.png",
+    description:
+      "Open your heart to this Aurora Destination Nation plush toy. This sweetie loves our school almost as much as you do. Plush toy arrives wearing a bandana printed with our school logo, and is ready to jump in your arms at a moment's notice. Plush toy measures approx. 12''.",
+    brand: "Spirit",
+    category: "Merchandise",
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
---
+++ server.js
@@ -0,0 +1,67 @@
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import path from "path";
+import cors from "cors";
+
+dotenv.config();
+
+// connect the db from mongoose
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.originalUrl);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Api is running..");
+});
+
+app.use(cors());
+
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+
+const __dirname = path.resolve();
+
+console.log(process.env.NODE_ENV === "production");
+if (process.env.NODE_ENV === "production") {
+  console.log("prod");
+
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 4000;
+// change port to 5000 when merging
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.cyan.bold
+  )
+);
