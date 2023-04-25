5d8579546830f56d0ec0d19aaa46ef7687b1d11d
Parth Manaktala <manaktala.parth@gmail.com>
2023-04-24 20:59:29 -0400

    Fixed a bug causing paypal to not render

--- server.js
+++ server.js
@@ -34,28 +34,20 @@ app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 
-const __dirname = path.resolve();
-
-console.log(process.env.NODE_ENV === "production");
-if (process.env.NODE_ENV === "production") {
-  console.log("prod");
-
-  app.use(express.static(path.join(__dirname, "/client/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running....");
-  });
-}
-
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
 app.use(errorHandler);
+
+if (process.env.NODE_ENV === "production") {
+  //*Set static folder up in production
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 4000;
 // change port to 5000 when merging
